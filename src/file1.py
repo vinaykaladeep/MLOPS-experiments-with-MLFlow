0a560dab89aacdeac3418f5ffd965cfabaf13377
vinaykaladeep <vinaykaladeep1@gmail.com>
2025-11-22 23:12:53 +0530

    py code contains code to create exp without logging extra artificats

---
+++ src/file1.py
@@ -0,0 +1,38 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import dagshub
+dagshub.init(repo_owner='vinaykaladeep', repo_name='MLOPS-experiments-with-MLFlow', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/vinaykaladeep/MLOPS-experiments-with-MLFlow.mlflow")
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 15
+n_estimators = 10
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
+
+    print(accuracy)
